d63e23c72ade231f9ca3c3c5fa6686328cb942f7
h-genie <ses2000hj@hanmail.net>
2022-05-08 14:42:34 +0900

    pref : 페이지네이션 모듈 분리

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import { getPatientList } from './API';
 import './App.css';
+import Pagination from './Pagination';
 
 const App = () => {
     useEffect(() => {
@@ -12,11 +13,6 @@ const App = () => {
     const [totalLength, setTotalLength] = useState(null);
     const [page, setPage] = useState(1);
 
-    const [length, setLength] = useState(10);
-    const [shownPaging, setShownPaging] = useState(Math.floor((page - 1) / 10) * 10);
-    const totalPage = totalLength && totalLength / length;
-    const [onlyNumber, setOnlyNumber] = useState(length);
-
     const callGetPatientListAPI = async (length, page) => {
         const response = await getPatientList(length, page);
         Promise.all([
@@ -25,54 +21,6 @@ const App = () => {
             setPage(response.page)
         ])
     }
-
-    const pagination = () => {
-        let arr = [];
-
-        shownPaging !== 0 && arr.push(
-            <li
-                key='prev'
-                onClick={() => setShownPaging(shownPaging - 10)}
-            >
-                ←
-            </li>
-        )
-
-        for (let i = shownPaging; i < Math.min(shownPaging + 10, totalPage); i++) {
-            arr.push(
-                <li
-                    key={i}
-                    onClick={() => callGetPatientListAPI(length, i + 1)}
-                >
-                    {i + 1}
-                </li>
-            );
-        }
-
-        shownPaging + 10 < totalLength / length && arr.push(
-            <li
-                key='next'
-                onClick={() => setShownPaging(shownPaging + 10)}
-            >
-                →
-            </li>
-        )
-
-        return arr;
-    };
-
-    const changeLength = e => {
-        e.preventDefault();
-
-        let newLenth;
-        if (!e.target[0].value || e.target[0].value === '0') newLenth = length;
-        else newLenth = parseInt(e.target[0].value);
-
-        setLength(newLenth);
-        callGetPatientListAPI(newLenth, 1);
-    }
-
-    const validatNumber = e => setOnlyNumber(Math.min(e.target.value.replace(/[^0-9]/g, ''), totalLength));
 
     return (
         <div className='table-container'>
@@ -110,15 +58,11 @@ const App = () => {
 
             </table>
 
-            <div className='pagination'>
-                <form onSubmit={changeLength}>
-                    <input type="text" onChange={validatNumber} value={onlyNumber} />
-                    <button type='submit'>변경</button>
-                </form>
-                <ul>
-                    {pagination()}
-                </ul>
-            </div>
+            <Pagination
+                callGetPatientListAPI={callGetPatientListAPI}
+                totalLength={totalLength}
+                page={page}
+            />
         </div>
     )
 }
---
+++ client/src/Pagination.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+
+const Pagination = (props) => {
+    const {
+        callGetPatientListAPI,
+        totalLength,
+        page
+    } = props;
+
+    const [length, setLength] = useState(10);
+    const [newLength, setNewLength] = useState(length);
+    const [shownPagination, setShownPagination] = useState(Math.floor((page - 1) / 10) * 10);
+
+    const changeLength = e => {
+        e.preventDefault();
+        let value = e.target[0].value;
+
+        if (!value || value === '0') value = length;
+        else value = parseInt(value);
+
+        setLength(value);
+        callGetPatientListAPI(value, 1);
+    }
+
+    const validatNumber = e => setNewLength(Math.min(e.target.value.replace(/[^0-9]/g, ''), totalLength));
+
+    const pagination = () => {
+        let arr = [];
+        const lastPage = Math.ceil(totalLength / length);
+
+        shownPagination !== 0 && arr.push(
+            <li
+                key='prev'
+                onClick={() => {
+                    setShownPagination(shownPagination - 10);
+                    callGetPatientListAPI(length, shownPagination - 9);
+                }}
+            >
+                ←
+            </li>
+        )
+
+        for (let i = shownPagination; i < Math.min(shownPagination + 10, lastPage); i++) {
+            arr.push(
+                <li
+                    key={i}
+                    onClick={() => callGetPatientListAPI(length, i + 1)}
+                    style={{
+                        color: i + 1 === page ? 'blue' : null,
+                        textDecoration: i + 1 === page ? 'underline' : null
+                    }}
+                >
+                    {i + 1}
+                </li>
+            );
+        }
+
+        shownPagination + 10 <= lastPage && arr.push(
+            <li
+                key='next'
+                onClick={() => {
+                    setShownPagination(shownPagination + 10);
+                    callGetPatientListAPI(length, shownPagination + 11);
+                }}
+            >
+                →
+            </li>
+        )
+
+        return arr;
+    };
+
+    return (
+        <div className='pagination'>
+            <form onSubmit={changeLength}>
+                <input
+                    type="text"
+                    onChange={validatNumber}
+                    value={newLength}
+                />
+                <span style={{ margin: '0px 10px' }}> / {totalLength}</span>
+                <button type='submit'>변경</button>
+            </form>
+            <ul>
+                {pagination()}
+            </ul>
+        </div>
+    )
+}
+
+export default Pagination;
